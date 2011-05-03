b70b3feccc1917dc88f422844d38fbd7ed68e098
Lucas Suggs <lucas.suggs@gmail.com>
2011-05-03 13:18:30 -0700

    Initial files.

---
+++ seclogon.cpp
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------
+//  File name: seclogon.cpp
+//  Author: Lucas Suggs (lucas.suggs@gmail.com)
+//
+//  This program is intended to parse either the system Security event log
+//  or a provided archived Security event log to pull out the interactive
+//  logon, logoff and screen unlock events for normal users.
+// ----------------------------------------------------------------------------
+
+
+// Preprocessor directives.
+#include <Windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+#define VERSION_MAJOR	0
+#define VERSION_MINOR	1
+
+
+// ----------------------------------------------------------------------------
+//  Name: _tmain
+//
+//  Desc: Application entry point.
+// ----------------------------------------------------------------------------
+int _tmain( int argc, TCHAR** argv )
+{
+	return 0;
+}
